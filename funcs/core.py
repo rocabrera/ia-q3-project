5e0889d9d3ba2511668ab3552c3b29edef28e084
Gigeck <guilherme.gigeck@gmail.com>
2021-10-24 22:27:49 -0300

    Refactoring, model movido para device, final report apresenta arquitetura

--- funcs/core.py
+++ funcs/core.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import numpy as np
 from torch.utils.data import DataLoader
 from funcs.model import MLP
-
+from typing import Tuple
 
 class Evaluate():
 
@@ -15,21 +15,22 @@ class Evaluate():
                  test_loader: DataLoader,
                  parameters: tuple):
 
-        self.model = model
+        self.num_epochs, self.criterion, self.optimizer, self.device = parameters
+        self.model = model.to(self.device)
         self.train_loader = train_loader
         self.eval_loader = eval_loader
         self.test_loader = test_loader
         self.n_total_steps_train = len(train_loader)
         self.n_total_steps_eval = len(eval_loader)
 
-        self.num_epochs, self.criterion, self.optimizer, self.device = parameters
+        
 
         # Pensar onde vão esses parêmtros depois
-        self.history_depth = 10
-        self.acc_eval_hist = np.zeros(self.history_depth, dtype=np.float32)
-        self.loss_eval_hist = np.zeros(self.history_depth, dtype=np.float32)
+        self.eval_history_depth = 10
 
     def train(self):
+        acc_eval_hist = np.zeros(self.eval_history_depth, dtype=np.float32)
+        loss_eval_hist = np.zeros(self.eval_history_depth, dtype=np.float32)
         for epoch in range(self.num_epochs):
             # enumerate mini batches
             for i, (inputs, targets) in enumerate(self.train_loader):
@@ -50,54 +51,43 @@ class Evaluate():
             # epoch end
 
             # eval
-            with torch.no_grad():
-                accumulated_loss = 0
-                n_corrects = 0
-                n_samples = 0
-                for i, (inputs, targets) in enumerate(self.eval_loader):
-                    inputs = inputs.to(self.device)
-                    targets = targets.to(self.device, dtype=torch.int64)
-                    yhat = self.model(inputs)
-                    loss = self.criterion(yhat, targets)
-                    accumulated_loss += loss
-                    _, prediction = torch.max(yhat, 1)
-                    n_samples += targets.shape[0]
-                    n_corrects += (prediction == targets).sum().item()
-                # entao, isso abaixo eh ruim pois a ultima batch talvez nao seja de 1500
-                # nao eh a media mais contente que eu faria, "but it is fine for now"
-                average_eval_loss = accumulated_loss / self.n_total_steps_eval
-                acc = n_corrects / n_samples
+            acc,average_eval_loss = self._fazer_eval()
+            if (epoch >= self.eval_history_depth):
+                # aqui faz sentido falar de
+                mean_loss = np.mean(loss_eval_hist)
+                mean_acc = np.mean(acc_eval_hist)
+                condition = False
+                # podemos trocar condition para ser alguma comparacao entre acc e mean_acc ou o outro par
+                if condition:
+                    break  # stop training
 
-                if (epoch >= self.history_depth):
-                    # aqui faz sentido falar de
-                    mean_loss = np.mean(self.loss_eval_hist)
-                    mean_acc = np.mean(self.acc_eval_hist)
-                    condition = False
-                    # podemos trocar condition para ser alguma comparacao entre acc e mean_acc ou o outro par
-                    if condition:
-                        break  # leaves
+            acc_eval_hist[epoch % self.eval_history_depth] = acc
+            loss_eval_hist[epoch % self.eval_history_depth] = average_eval_loss
 
-                self.acc_eval_hist[epoch % self.history_depth] = acc
-                self.loss_eval_hist[epoch % self.history_depth] = average_eval_loss
+    def test_using(self, loader:DataLoader) -> Tuple[float,float]:
+        with torch.no_grad():
+            accumulated_loss = 0
+            n_corrects = 0
+            n_samples = 0
+            for i, (inputs, targets) in enumerate(loader):
+                inputs = inputs.to(self.device)
+                targets = targets.to(self.device, dtype=torch.int64)
+                yhat = self.model(inputs).to(self.device)
+                loss = self.criterion(yhat, targets)
+                accumulated_loss += loss
+                _, prediction = torch.max(yhat, 1)
+                n_samples += targets.shape[0]
+                n_corrects += (prediction == targets).sum().item()
+            # entao, isso abaixo eh ruim pois a ultima batch talvez nao seja de 1500
+            # nao eh a media mais contente que eu faria, "but it is fine for now"
+            average_loss = accumulated_loss / len(loader)
+            acc = n_corrects / n_samples
+            return (acc,average_loss)
 
-#    def _fazendo_evaluate()
+    def _fazer_eval(self) -> Tuple[float,float]:
+        return self.test_using(self.eval_loader)
 
     def report_final_result(self):
         # testagem final
-        with torch.no_grad():
-            n_corrects = 0
-            n_samples = 0
-            for i, (inputs, targets) in enumerate(self.test_loader):
-                inputs = inputs.to(self.device)
-                # print(inputs)
-                targets = targets.to(self.device, dtype=torch.int64)
-                outputs = self.model(inputs)
-                # print(outputs)
-
-                _, prediction = torch.max(outputs, 1)
-                # print(prediction)
-                n_samples += targets.shape[0]
-                n_corrects += (prediction == targets).sum().item()
-
-            acc = 100 * n_corrects / n_samples
-            print(f'accuracy equals {acc}')
+        acc, average_loss = self.test_using(self.test_loader)
+        print((repr(self.model) + f'; Desempenho: accuracy equals {acc:.3f}; average loss equals {average_loss:.3f}'))
