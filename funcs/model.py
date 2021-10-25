5be4f28679aa1b5f85aca1f82cd8c03d119ac633
Gigeck <guilherme.gigeck@gmail.com>
2021-10-24 22:24:31 -0300

    Adicionado custom repr, adicionado arch como atributo

--- funcs/model.py
+++ funcs/model.py
@@ -5,19 +5,20 @@ class MLP(nn.Module):
 
     def __init__(self, model_arch):
         super(MLP, self).__init__()
+        self.arch = model_arch
         self.camadas = nn.ModuleList([nn.Linear(before, after)
                                       for before, after in model_arch.parse_architecture()])  # achar nomes melhores
 
         self.relu = nn.ReLU()
 
     def forward(self, X):
-        """
-        Precisa ter esse out inicializando fora do for ?
-        """
+        
         out = self.camadas[0](X)
-
         for camada in self.camadas[1:]:
             out = self.relu(out)
             out = camada(out)
 
         return out
+    
+    def __repr__(self) -> str:
+        return ('Arch:' + repr(self.arch) + ', activate: ' + repr(self.relu))
