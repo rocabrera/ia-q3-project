148b0dcca82f45f04d3d23683b0af0d4f0b64680
Gigeck <guilherme.gigeck@gmail.com>
2021-10-27 22:42:26 -0300

    Modificado para trabalhar com Trainer/Classificador

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # lib imports
 import os
+from torch.optim import optimizer
 from tqdm import tqdm
 import pandas as pd
 import torch
@@ -10,7 +11,7 @@ from src.model.model import MLP
 from src.processing.Loader_Factory import Loader_Factory
 import src.processing.raw_data_processing as rdp
 from src.model.architecture import ModelArchitecture
-from src.core import Evaluate
+from src.core import Trainer, Classificador
 
 # Spliting data in Train/Eval/Test
 df = rdp.get_dataframe_from(os.path.join("data", "Dry_Bean_Dataset.xlsx"))
@@ -24,6 +25,9 @@ factory = Loader_Factory(batch_size)
 test_loader = factory.create_loader(test_pack)
 eval_loader = factory.create_loader(eval_pack)
 train_loader = factory.create_loader(train_pack)
+# training definition
+num_epochs = 100
+coach = Trainer(train_loader,eval_loader,test_loader,num_epochs)
 
 ##############################################################################
 
@@ -43,22 +47,30 @@ optimizers = [torch.optim.Adam(
     model.parameters(), lr=learn_rate) for model in models]
 
 # training definition
-num_epochs = 100
+#num_epochs = 100
 
 # TODO Tem problema não instanciar um nn.CrossEntropy por modelo?
-models_parameters = [(num_epochs, criterion, optimizer)
-                     for optimizer in optimizers]
+#models_parameters = [(num_epochs, criterion, optimizer)
+#                     for optimizer in optimizers]
+
+classificadores = [Classificador(model,criterion,opti) 
+                    for (model,opti) in zip(models,optimizers)]
+
+results = []
+for trainee in tqdm(classificadores, total=len(classificadores)):
+    coach.train(trainee)
+    results.append(coach.report(trainee))
 
 """
 # TODO Esse evaluate está fazendo muita coisa. 
 # Talvez no futuro separa em duas classes onde uma treina e outra prediz
 """
 
-results = []
 
-for model, parameters in tqdm(zip(models, models_parameters), total=len(models)):
-    ev = Evaluate(model, train_loader, eval_loader, test_loader, parameters)
-    ev.train()
-    results.append(ev.report())
+
+#for model, parameters in tqdm(zip(models, models_parameters), total=len(models)):
+#    ev = Evaluate(model, train_loader, eval_loader, test_loader, parameters)
+#    ev.train()
+#    results.append(ev.report())
 
 print(pd.concat(results, axis=1).T)
